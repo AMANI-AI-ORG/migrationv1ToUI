0376fc8c6dc1a5d5c4f9ef3bad2704e6414d2274
bbedry <bedri@amani.tech>
2025-06-11 15:19:06 +0300

    added gitignore

--- Pods/Target Support Files/AmaniUI/AmaniUI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AmaniUI : NSObject
-@end
-@implementation PodsDummy_AmaniUI
-@end
--- Pods/Target Support Files/Pods-migrationv1toUI/Pods-migrationv1toUI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_migrationv1toUI : NSObject
-@end
-@implementation PodsDummy_Pods_migrationv1toUI
-@end
